cf89c5fe6cfa7c7964c0197d31f5b941a9471f0b
buianhduc <buianhduc.popodoo2@gmail.com>
2020-09-05 20:17:21 +0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # fav-menu
+
+Để chạy file mọi ng nhập lệnh 
+<code>python app.js</code>
--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, request
 import psycopg2
-app = Flask(__name__)
+import os
+app = Flask(__name__,template_folder='template',static_folder='static')
+
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                 endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
 
 @app.route('/')
 def index():
-    return "Its works"
+    return render_template('index.html')
 
 app.run()
