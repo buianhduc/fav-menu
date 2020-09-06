714d3a8a06a10040248d45cf3c830f3d61fc777c
buianhduc <buianhduc.popodoo2@gmail.com>
2020-09-06 16:46:54 +0700

    final

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, redirect, render_template, request
 import psycopg2
+import json
 import os
 app = Flask(__name__,template_folder='template',static_folder='static')
 
@@ -19,5 +20,74 @@ def dated_url_for(endpoint, **values):
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/survey', methods = ['GET','POST'])
+def survey():
+    if request.method == 'GET':
+        return render_template('survey.html')
+    elif request.method == 'POST':
+        print('hello')
+        form = request.form
+        # print(form)
+        # answer1 = form['question1_1']
+        # answer1,answer2,answer3 = form['selectionBox1'],form['selectionBox2'],form['selectionBox3']
+        f = open('static/js/answers.json','r')
+        answer = json.load(f);
+        genre_test = [
+            {
+                "name" : "edm",
+                "status" : False
+            },
+            {
+                "name" : "pop",
+                "status" : False
+            },
+            {
+                "name" : "soul",
+                "status" : False
+            },
+        ]
+        country_test = [
+            {
+                "name" : "vn",
+                "status" : False
+            },
+            {
+                "name" : "us",
+                "status" : False
+            },
+            {
+                "name" : "hq",
+                "status" : False
+            }
+        ]
+        
+        res = []
+        res_temp = []
+        country = []
+        genre = []
+        
+        for i in form:
+            if i == 'vn' or i == 'us' or i == 'hq':
+                for j in country_test:
+                    if i == j['name']:
+                        country.append(i)
+            elif i == 'pop' or i == 'soul' or i == 'edm':
+                for j in genre_test:
+                    if i == j['name']:
+                        genre.append(i)
+        print(country)
+        print(genre)
+        for i in country:
+            for j in answer:
+                if j['country'] == i:
+                    res_temp.append(j)
+        for i in genre:
+            for j in res_temp:
+                if j['genre'] == i:
+                    res.append(j)          
+        print(res)
+        return render_template('result.html',items = res)
+
 
 app.run()
--- static/js/generateQuestions.js
+++ static/js/generateQuestions.js
@@ -9,22 +9,63 @@ function readJSONFile(file, callback) {
     };
     rawFile.send(null);
 }
-
+var answers, questions;
 readJSONFile("../js/answers.json", function (text) {
     var data = JSON.parse(text);
-    for (let i in data) {
-        if (data[i].country === "Việt Nam") {
-            var ans = "<li>" + data[i].name + "</li>";
-            $(".choices").append(ans);
-        }
-    }
+    answers = data;
 
     // console.log(data.length)
 });
-$("document").ready(function () {
-    $("button").click(function(){
-        $.ajax({url: "../lol.txt", success: function(result){
-            $("#question").html(result);
-        }});
-    });
-});
+questions = [
+    {
+        "id": 1,
+        "text": "what are your favourite music?",
+        "choices": [
+            "Pop",
+            "R&B/Soul",
+            "EDM"
+        ]
+    },
+    {
+        "id": 1,
+        "text": "where do your favourite music come from ?",
+        "choices": [
+            "Việt Nam",
+            "US-UK",
+            "Hàn Quốc"
+        ]
+    },
+    {
+        "id": 1,
+        "text": "what are your favourite artists?",
+        "choices": []
+    }
+];
+
+
+
+// $("document").ready(function () {
+//     for (var question in questions) {
+//         if (questions[question].id !== 3) {
+//             $("#question-container").append(`
+//             <div class = 'container-fluid bg-white rounded mb-0' style = 'margin-top: 5%; padding-top: 10px; padding-left: 30px;'>
+//                 <div class='question-and-number'>
+//                 <div class='row question-number'>Question ${Number(question) + 1}/3</div>
+//                 <div class='row question'>
+//                     <h1>${questions[question].text}</h1>
+//                 </div>
+//                 <div class='row form-check'>
+//                 </div>
+//             </div>
+//             </div>
+//             `);
+//             // $("document").ready(function() {
+//                 for (var i in questions[question].choices) {
+//                     var html = `<input type="checkbox" class="form-check-input" id="materialUnchecked">
+//                     <label class="form-check-label" for="materialUnchecked">${questions[question].choices[i]}</label>`
+//                     $('.form-check')[question].append(html);
+//                 }
+//             // });
+//         }
+//     }
+// });
